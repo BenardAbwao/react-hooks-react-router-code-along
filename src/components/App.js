a352b21f0066582268378b255a9c28cd17c2fc31
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-20 09:31:38 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
 import Login from "./Login";
 import Home from "./Home";
-import Navbar from "./Navbar";
+import NavBar from "./NavBar";
 import About from "./About";
-import { Switch , Route} from "react-router-dom";
+import { Route , Routes} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Switch>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-      <Route exact path='/about'>
-        <About/>
-      </Route >
-      <Route exact path='/login'>
-        <Login />
-      </Route>
-    </Switch>
-  </>
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </div>
   );
 }
 
